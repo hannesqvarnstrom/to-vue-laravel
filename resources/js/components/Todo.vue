848d2c70027674f199a895fbac02e28c24678401
hannesqvarnstrom <hannesqvarnstrom@gmail.com>
2021-10-08 10:26:57 +0200

    sets up basic stuff, adds a vue component, adds some migrations for todos and lists

---
+++ resources/js/components/Todo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h5>{{ title }}</h5>
+    <small>{{ description }}</small>
+    <button @click="complete">{{ done ? "Done" : "Not done" }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {
+    complete() {
+      axios.get("http://localhost/api/test").then((data) => {
+        console.log(data);
+      });
+    },
+  },
+  props: {
+    title: String,
+    description: String,
+    done: Boolean,
+  },
+};
+</script>
